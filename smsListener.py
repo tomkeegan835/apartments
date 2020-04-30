2cb25bb4b627288ce7b952bdaf50e4c5797b5f1f
Tom Keegan <tomkeegan835@gmail.com>
2020-04-30 00:28:10 -0700

    ran into error trying to download, edit and upload file to s3

--- smsListener.py
+++ smsListener.py
@@ -1,9 +1,18 @@
-import json
+import json, boto3
 
-def lambda_handler(event, context):
-    
-    print('EVENT:\n\n',event,'\n\n')
-    
+s3 = boto3.client('s3')
+
+def smsHandler(event, context):
+
+    print(event)
+
+    url = (json.loads(event['Records'][0]['Sns']['Message'])["messageBody"])
+
+    with open('listingUrls.txt', 'wb') as listingUrlsFile:
+        s3.download_fileobj('com.tom.apartments', 'listingUrls.txt', listingUrlsFile)
+        listingUrlsFile.write(url)
+        s3.upload_fileobj(listingUrlsFile, 'com.tom.apartments', 'listingUrls.txt')
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
