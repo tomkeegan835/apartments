03ae6f7269d3308ead889240590fdc29b992eef5
Tom Keegan <tomkeegan835@gmail.com>
2020-05-02 02:43:32 -0700

    created dockerfile for sqlite

--- smsListener.py
+++ smsListener.py
@@ -1,18 +1,11 @@
-import json, boto3
-
-# create s3 client
-s3 = boto3.client('s3')
+import json
 
 def smsHandler(event, context):
 
-    # print event for logging purposes
-    print(json.dumps(event))
-
     # get url from event
-    url = (json.loads(event['Records'][0]['Sns']['Message'])["messageBody"])
+    url = json.loads(event['Records'][0]['Sns']['Message'])["messageBody"]
 
-    with open('listingUrls.txt', 'wb') as listingUrlsFile:
-        s3.download_fileobj('com.tom.apartments', '/tmp/listingUrls.txt', listingUrlsFile)
+    
 
     return {
         'statusCode': 200,
