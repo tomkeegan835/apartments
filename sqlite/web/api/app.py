4a69c209c4482cfce20858523a7b2aa9e427f68c
Tom Keegan <tomkeegan835@gmail.com>
2020-05-03 02:34:18 -0700

    made sqlite flask api but can't figure out how to view it from outside the container

---
+++ sqlite/web/api/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import sqlite3
+
+api = Flask(__name__)
+
+@api.route('/', methods=['GET'])
+def hello():
+    return "Hello world"
+
+@api.route('/urls/<url>', methods=['POST'])
+def write_url(url):
+    urls = sqlite3.connect('urls.db')
+    cursor = urls.cursor()
+    urls.execute('CREATE  TABLE [IF NOT EXISTS] urls')
+    urls.execute('INSERT INTO urls VALUES ' + url)
+
+if __name__ == '__main__':
+    api.run(host="0.0.0.0", port="80")
