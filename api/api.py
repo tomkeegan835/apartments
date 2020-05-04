bc27e79fdbf3590f921ced7f6d6e1ba2aa406042
Tom Keegan <tomkeegan835@gmail.com>
2020-05-04 00:25:19 -0700

    just can't get flask to parse the POST request

--- api/api.py
+++ api/api.py
@@ -1,18 +1,29 @@
-from flask import Flask
-import sqlite3
+from flask import Flask, request, url_for
+import sqlite3, sys
 
+# create the flask api
 api = Flask(__name__)
+
+# create the urls table
+listings = sqlite3.connect('listings.db')
+listings.execute('''CREATE TABLE IF NOT EXISTS listings (
+                        url text PRIMARY KEY
+                    );''')
 
 @api.route('/', methods=['GET'])
 def hello():
-    return "Hello world"
 
-@api.route('/urls/<url>', methods=['POST'])
-def write_url(url):
-    urls = sqlite3.connect('urls.db')
-    cursor = urls.cursor()
-    urls.execute('CREATE  TABLE [IF NOT EXISTS] urls')
-    urls.execute('INSERT INTO urls VALUES ' + url)
+    return "Hello"
+
+@api.route('/urls/<listingUrl>', methods=['POST'])
+def write_url(listingUrl):
+    print(listingUrl, file=sys.stdout)
+    listings.execute('INSERT INTO listings VALUES ' + listingUrl)
+    print('created record with url', file=sys.stdout)
+    return 'created record with url'
+
+with api.test_request_context():
+    print(url_for('write_url', listingUrl='http://www.google.com'))
 
 if __name__ == '__main__':
-     api.run(host="0.0.0.0", port=80)
+     api.run(host="0.0.0.0", port=80, debug=True)
--- lambda/smsListener.py
+++ lambda/smsListener.py
@@ -1,10 +1,16 @@
 import json
+import urllib3
 
 def smsHandler(event, context):
 
     # get url from event
-    url = json.loads(event['Records'][0]['Sns']['Message'])["messageBody"]
+    listingUrl = json.loads(event['Records'][0]['Sns']['Message'])["messageBody"]
 
+    http = urllib3.PoolManager()
+
+    response = http.request(method='POST',url='http://54.188.29.69/urls/',fields={'listingUrl': listingUrl})
+
+    print(response.data)
 
     return {
         'statusCode': 200,
