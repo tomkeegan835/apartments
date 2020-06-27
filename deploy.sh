f19af901e160d3d45e53ff1984fdd9040477f0dc
Tom Keegan <tomkeegan835@gmail.com>
2020-06-26 21:49:04 -0700

    fixed stuff

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-zip package.zip api.py craigslist.py crawler.py requirements.txt sql.py util.py crawl.py
-sftp -i ../../api-server.pem ubuntu@ec2-52-27-61-84.us-west-2.compute.amazonaws.com <<EOF
-put package.zip
