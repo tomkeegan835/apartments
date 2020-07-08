cf3e5c29caa1828eeb0ec844d963823deb57dc0a
Tom Keegan <tomkeegan835@gmail.com>
2020-07-07 22:33:31 -0500

    split mke and sf crawl scripts

---
+++ milwaukee.py
@@ -0,0 +1,17 @@
+import craigslist, crawler, sql
+
+while(True):
+    fetchedUrls = crawler.crawl('https://milwaukee.craigslist.org/search/apa?', 3000)
+
+    sql.craigslist_create('listingsCrawlMilwaukee')
+
+    currentUrls = set()
+    for row in sql.column('listingsCrawlMilwaukee', 'url'):
+        currentUrls.add(row[0])
+
+    newUrls = fetchedUrls - currentUrls
+
+    if(len(newUrls) > 0):
+        craigslist.scrape_set(newUrls, 'listingsCrawlMilwaukee')
+
+    crawler.clean('listingsCrawlMilwaukee')mi
